f42ac434833f51a5948bcc2600cbc3f0a4d940fe
Marcus Le <mle10@uoguelph.ca>
2025-10-22 11:40:37 -0400

    Add Fibonacci sequence function with user input
    Implement Fibonacci sequence generator with input validation.

--- fibonacci.py
+++ fibonacci.py
@@ -2,3 +2,22 @@
 
 # Fibonacci Sequence Exercise with functions
 # TODO: (Read detailed instructions in the Readme file)
+
+def fibonacci():
+    while True:
+        amount = input()
+        if amount.isalpha() == True or int(amount) <= 0:
+            print("Please enter a positive integer.")
+        else:
+            fib = "0 1 "
+            first = 0
+            second = 1
+            for i in range(int(amount) - 2):
+                fib += str(first + second) + " "
+                temp = first + second
+                first = second
+                second = temp
+            print(fib)
+            
+
+fibonacci()
