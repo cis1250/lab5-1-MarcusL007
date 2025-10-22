bee4e630d84e921823275b1988819b0f6b296059
Marcus Le <mle10@uoguelph.ca>
2025-10-22 12:16:26 -0400

    Add functions for validating and calculating word frequencies

--- word_frequency.py
+++ word_frequency.py
@@ -24,3 +24,41 @@ def is_sentence(text):
         return False
 
     return True
+
+def isValid():
+    user_sentence = input("Enter a sentence: ")
+    while (is_sentence(user_sentence) == False):
+        print("This does not meet the criteria for a sentence.")
+        user_sentence = input("Enter a sentence: ")
+    user_sentence = re.sub(r'[^A-Za-z ]', '', user_sentence)
+    return user_sentence
+
+def calculate_frequencies(user_sentence):
+    sentence = user_sentence.split(" ")
+
+    words = []
+    count = []
+
+    for word in sentence:
+        if word not in words:
+            words.append(word.lower())
+            count.append(0)
+    for word in sentence:
+        index = words.index(word.lower())
+        count[index] += 1
+
+    return words, count
+
+def print_frequencies(words, frequencies):
+    for i in range(len(words)):
+        print(words[i] + ": " + str(frequencies[i]))
+
+def main():
+    user_sentence = isValid()
+    
+    words, count = calculate_frequencies(user_sentence)
+
+    print_frequencies(words, count)
+    
+
+main()
