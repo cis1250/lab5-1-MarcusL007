7e3421f6738c347e3e636e76c4a4645c5e40d965
Marcus Le <mle10@uoguelph.ca>
2025-10-22 11:59:40 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -3,21 +3,29 @@
 # Fibonacci Sequence Exercise with functions
 # TODO: (Read detailed instructions in the Readme file)
 
+def validCheck(amount):
+    if amount.isalpha() == True or int(amount) <= 0:
+        print("Please enter a positive integer.")
+        return True
+    else:
+        return False
+        
+def generate(amount):
+    fib = "0 1 "
+    first = 0
+    second = 1
+    for i in range(int(amount) - 2):
+        fib += str(first + second) + " "
+        temp = first + second
+        first = second
+        second = temp
+    return fib
+
 def fibonacci():
     while True:
         amount = input()
-        if amount.isalpha() == True or int(amount) <= 0:
-            print("Please enter a positive integer.")
-        else:
-            fib = "0 1 "
-            first = 0
-            second = 1
-            for i in range(int(amount) - 2):
-                fib += str(first + second) + " "
-                temp = first + second
-                first = second
-                second = temp
-            print(fib)
+        if not validCheck(amount):
+            print(generate(amount))
             break
-            
+
 fibonacci()
